34d077a226d58d902de7ede294af448afc159dfd
Anamaya1729 <73896949+Anamaya1729@users.noreply.github.com>
2021-09-18 13:08:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ function setup() {
   tree = new BST()
   background(30);
   translate(width/2, 16);
-  for (let i = 0; i < 16; i++)
+  for (let i = 0; i < 10; i++)
   {
     tree.Insert(Math.floor(random(1,100)))
   }
